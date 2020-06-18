537d698512acaf6754419288bfea50eba3e8d130
KitaharaMugiro <ngthornym@yahoo.co.jp>
2020-06-18 09:00:58 +0900

    Initial Commit

---
+++ NegotoRecorder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NegotoRecorder
+//
+//  Created by kitaharamugirou on 2020/06/18.
+//  Copyright © 2020 kitaharamugirou. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
