85cfc38f6f4461b9ebe152a1a99787abb386cc77
KitaharaMugiro <ngthornym@yahoo.co.jp>
2020-06-22 13:52:44 +0900

    aaa

--- Pods/Target Support Files/NeumorphismTab/NeumorphismTab-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_NeumorphismTab : NSObject
-@end
-@implementation PodsDummy_NeumorphismTab
-@end
--- Pods/Target Support Files/Pods-NegotoRecorder/Pods-NegotoRecorder-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_NegotoRecorder : NSObject
-@end
-@implementation PodsDummy_Pods_NegotoRecorder
-@end
